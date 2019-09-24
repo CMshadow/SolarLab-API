09eb74d81eaf2147f82fcf98d05cf8192ecb9d4a
CMshadow <bjwmc1996@gmail.com>
2019-09-24 15:40:15 -0700

    FIX accident delete

---
+++ setback-polyline-calculation/function/app.js
@@ -0,0 +1,30 @@
+const FoundLine = require('./infrastructure/line/foundLine');
+
+exports.lambdaHandler = async (event, context) => {
+  const originPolyline = FoundLine.fromPolyline(event.originPolyline);
+  const stbDist = event.stbDist;
+  const direction = event.direction;
+
+  let stbPolylines = [];
+  if (direction === 'inside') {
+    stbPolylines = originPolyline.makeSetbackPolylineInside(stbDist);
+  } else {
+    stbPolylines = originPolyline.makeSetbackPolylineOutside(stbDist);
+  }
+  console.log(stbPolylines)
+
+  let response;
+  try {
+    response = {
+      'statusCode': 200,
+      'body': JSON.stringify({
+        stbPolylines: stbPolylines,
+      })
+    }
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response
+};

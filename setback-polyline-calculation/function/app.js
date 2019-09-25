abdac0b3192fd86fee6be325ae0fb55d1320ae1e
CMshadow <bjwmc1996@gmail.com>
2019-09-25 00:13:44 -0700

    ADD stbDist === 0 condition

--- setback-polyline-calculation/function/app.js
+++ setback-polyline-calculation/function/app.js
@@ -1,15 +1,20 @@
 const FoundLine = require('./infrastructure/line/foundLine');
 
 exports.lambdaHandler = async (event, context) => {
+  console.log(event)
   const originPolyline = FoundLine.fromPolyline(event.originPolyline);
   const stbDist = event.stbDist;
   const direction = event.direction;
 
   let stbPolylines = [];
-  if (direction === 'inside') {
-    stbPolylines = originPolyline.makeSetbackPolylineInside(stbDist);
+  if (stbDist === 0) {
+    stbPolylines = [originPolyline];
   } else {
-    stbPolylines = originPolyline.makeSetbackPolylineOutside(stbDist);
+    if (direction === 'inside') {
+      stbPolylines = originPolyline.makeSetbackPolylineInside(stbDist);
+    } else {
+      stbPolylines = originPolyline.makeSetbackPolylineOutside(stbDist);
+    }
   }
   console.log(stbPolylines)
 

acc098b36f008f59e970c97c86fb79ee550780de
CMshadow <bjwmc1996@gmail.com>
2019-11-11 15:01:56 -0800

    IMP get panel structure

--- getPanelAPP.js
+++ getPanelAPP.js
@@ -3,6 +3,10 @@ AWS.config.update({ region: 'us-west-1' });
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
 exports.lambdaHandler = async (event, context, callback) => {
+  console.log('==================EVENT===================');
+  console.log(event);
+  console.log('===================END====================');
+
   const userID = event.userID;
 
   const params = {
@@ -12,26 +16,27 @@ exports.lambdaHandler = async (event, context, callback) => {
       ':userID': { S: userID }
     }
   };
-  if (event.attributes !== '') params.ProjectionExpression = event.attributes;
+  if (event.attributes) {
+    params.ProjectionExpression = event.attributes.join(', ');
+  }
 
-  const queryItems = new Promise((resolve, reject) => {
-    dynamodb.query(params, (err, data) => {
-      if (err) {
-        console.log('Error', err);
-        reject(err);
+  let response = null;
+  await dynamodb.query(params, (err, data) => {
+    if (err) {
+      throw new Error('Error: Database error');
+    } else {
+      if (data.Items.length === 0) {
+        throw new Error('Error: User does not have any record');
       } else {
-        const formedData = data.Items.map(panel => {
+        response = data.Items.map(panel => {
           Object.keys(panel).forEach(k => {
             panel[k] = Object.values(panel[k])[0];
           });
           return panel;
         });
-        console.log('Success', formedData);
-        resolve(data);
       }
-    });
-  });
+    }
+  }).promise();
 
-  const result = await queryItems;
-  return result;
+  return response;
 };
